f8830d58029a418b5b1f9971e0aa7df86ab45fc6
Priyavv <43750304+Priyavv@users.noreply.github.com>
2020-12-09 22:35:23 +0530

    Add files via upload

---
+++ feedback.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE `feedback` (
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `mobile` int(10) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `feedback` varchar(255) NOT NULL,
+  `rating` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
---
+++ help.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `help` (
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `mobile` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `problem` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
